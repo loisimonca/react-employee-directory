2f979eeaaccfcf8d46ca13b7295832671ace84c0
Lois Simonca <loisgabor@gmail.com>
2021-01-28 12:19:37 -0500

    successfully popupated API results using the . map function

--- src/component/EmployeeCard/EmployeeCard.jsx
+++ src/component/EmployeeCard/EmployeeCard.jsx
@@ -2,55 +2,65 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const EmployeeCard = () => {
-  const [employees, setEmployee] = useState({
-    first: "",
-    last: "",
-    phone: "",
-    email: "",
-  });
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://randomuser.me/api/")
+      .get("https://randomuser.me/api/?results=50")
       .then((response) => {
-        // console.log(response.data.results);
-        setEmployee([response.data.results]);
+        console.log(response.data.results);
+        setUsers(response.data.results);
       })
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []);
 
+  // const sortByName = () => {
+  //   const sortedUser = users.sort((a, b) => {
+  //     const aValue = a.name.first;
+  //     const bValue = b.name.first;
+  //     if (aValue < bValue) {
+  //       return -1;
+  //     }
+  //     if (aValue > bValue) {
+  //       return 1;
+  //     }
+  //     return 0;
+  //   });
+  //   console.log(sortedUser);
+  // };
   return (
     <div className="container" style={{ marginTop: 100 }}>
-      <table class="table">
+      <table className="table table-dark table-striped">
         <thead>
           <tr>
             <th scope="col">ID</th>
             <th scope="col">Photo</th>
-            <th scope="col">Name</th>
+            <th scope="col" onClick={sortByName}>
+              Name
+            </th>
             <th scope="col">Email</th>
             <th scope="col">Phone</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <th scope="row">{user.id.value}</th>
+              <td>
+                <img
+                  src={user.picture.thumbnail}
+                  alt="{user.name.first} {user.name.last}"
+                ></img>
+              </td>
+              <td>
+                {user.name.first} {user.name.last}
+              </td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
